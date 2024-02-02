2ad9c7afb2b16f303332d55fc1b34527d0617945
Vlad Verheij <maciejkrp@gmail.com>
2024-02-02 21:34:43 +0100

    Les 2.3

---
+++ resources/views/home.blade.php
@@ -0,0 +1,10 @@
+@extends('layouts.app')
+
+@section('title', 'Home Pagina')
+
+@section('content')
+    <div class="content-element">
+        <h2>Welkom op de Home Pagina</h2>
+        <p>Dit is een voorbeeldtekst voor de homepagina. Voel je vrij om deze tekst aan te passen aan je eigen behoeften.</p>
+    </div>
+@endsection
---
+++ resources/views/includes/footer.blade.php
@@ -0,0 +1,3 @@
+<footer>
+    <p>&copy; {{ date('Y') }} Vlad Verheij. Alle rechten voorbehouden.</p>
+</footer>
